f0ededea78c591dea1f40e5fe66cf4f6a757dc6e
Sophia Hobie <sh01806@srcs.org>
2022-05-26 14:42:41 -0700

    Game is complete

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,14 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRange = 9;
     public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(3);
-       
+       SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
     }
     void SpawnEnemyWave(int enemiesToSpawn)
     {
@@ -37,7 +40,9 @@ public class SpawnManager : MonoBehaviour
 
         if(enemyCount == 0)
         {
-            SpawnEnemyWave(1);
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
         }
     }
 }
